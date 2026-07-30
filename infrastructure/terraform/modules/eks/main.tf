73b28e9875f351b432841a3b0759bb1dcd6541f3
Vu The Huy <vuthehuy.t61@gmail.com>
2026-07-30 23:52:18 +0700

    feat: update GitHub Actions IAM role policy to allow access for all branches in the repository

--- infrastructure/terraform/modules/eks/main.tf
+++ infrastructure/terraform/modules/eks/main.tf
@@ -332,7 +332,9 @@ resource "aws_iam_role" "github_actions_deploy" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:vuthehuyht/finops-data-stack:ref:refs/heads/main"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:vuthehuyht/finops-data-stack:*"
           }
         }
       }
